c9e79e5b5d049cdb6ac5f3a68b4bd8753380b5af
saberxxy <albert_saber@163.com>
2018-04-08 08:39:11 +0800

    添加建表语句

--- zhihu_topic/zhihu_get_topic.py
+++ zhihu_topic/zhihu_get_topic.py
@@ -23,102 +23,111 @@ import zhihu_spider.zhihu_login.Util as Util
 import zhihu_spider.zhihu_login.ThreadUtil as ThreadUtil
 
 
-# cookies = zhihu_login.get_cookie()
-# s = requests.session()
-# s.cookies = requests.utils.cookiejar_from_dict(cookies)
-s = zhihu_login.get_session()
-print(s)
+def get_topic_info(level, number):
+    # 获取session
+    s = zhihu_login.get_session()
+    # print("session：", s)
 
-hot_url = "https://www.zhihu.com/topic/19744646/hot"
-organize_url = "https://www.zhihu.com/topic/19744646/organize"
-parent_url = "https://www.zhihu.com/api/v3/topics/19744646/parent"
-children_url = "https://www.zhihu.com/api/v3/topics/19744646/children"
+    hot_url = "https://www.zhihu.com/topic/" + str(number) + "/hot"
+    organize_url = "https://www.zhihu.com/topic/" + str(number) + "/organize"
+    parent_url = "https://www.zhihu.com/api/v3/topics/" + str(number) + "/parent"
+    children_url = "https://www.zhihu.com/api/v3/topics/" + str(number) + "/children"
 
-# r_hot = s.get(url = hot_url, headers = Util.Default_Headers)
-# soup_hot = BeautifulSoup(r_hot.content, 'lxml')
-# print(soup_hot)
-# print(cookies)
+    # 来源于hot_url的数据
+    r_hot = s.get(url = hot_url, headers = Util.Default_Headers)
+    soup_hot = BeautifulSoup(r_hot.content, 'lxml')
+
+    # uuid
+    topic_uuid = str(uuid.uuid1())
+    print('UUID：', topic_uuid)
+
+    # 话题编号
+    a_1 = soup_hot.findAll('a', attrs={'class':'Tabs-link is-active'})
+    topic_id = str(re.findall(r'.*href="/topic/(.*)/hot', str(a_1))[0])
+    print('话题编号：', topic_id)
+
+    # 话题名称
+    a_2 = soup_hot.findAll('h1', attrs={'class':'TopicCard-titleText'})[0].contents[0]
+    topic_name = str(a_2)
+    print('话题名称：', topic_name)
+
+    # 话题链接
+    a_3 = soup_hot.findAll('a', attrs={'class':'Tabs-link is-active'})
+    topic_link = "https://www.zhihu.com" + \
+        str(re.findall(r'.*href="(.*)">', str(a_3))[0])
+    print('话题链接：', topic_link)
+
+    # 关注人数
+    a_4 = soup_hot.findAll('strong', attrs={'class':'NumberBoard-itemValue'})
+    follow_number = int(str(a_4[0].contents[0]).replace(',', ''))
+    print('关注人数：', follow_number)
+
+    # 问题数
+    a_5 = soup_hot.findAll('strong', attrs={'class':'NumberBoard-itemValue'})
+    question_number = int(str(a_5[1].contents[0]).replace(',', ''))
+    print('问题数：', question_number)
+
+    # 来源于organize_url的数据
+    # 话题别名
+    # r_organize = s.get(url = organize_url, headers = Util.Default_Headers)
+    # soup_organize = BeautifulSoup(r_organize.content, 'lxml')
+    # print(soup_organize)
+
+    # 话题描述
+    a_6 = soup_hot.findAll('span', attrs={'class':'RichText'})[0]
+    # print(a_6)
+    # topic_describe = str(re.findall(r'.*">(.*) http', str(a_6))[0])
+    topic_describe = str(a_6.contents[0])
+    print('话题描述：', topic_describe)
+
+    # 来源于parent_url的数据
+    # 父级话题和子级话题不是在页面加载的，需要要请求Ajax
+    # 父级话题编号【一个话题的父话题可能很多，所以存成列表】
+    parent_topic_id_list = []
+    r_parent = s.get(url = parent_url, headers = Util.Default_Headers)
+    jsondata = json.loads(re.match(".*?({.*}).*", r_parent.text, re.S).group(1))['data']
+    # print(jsondata)
+    for i in jsondata:
+        parent_topic_id_list.append(i['id'])
+    parent_topic_id = ','.join(parent_topic_id_list)
+    print('父级话题编号：', parent_topic_id)
+
+    # 来源于children_url的数据
+    # 子级话题编号【一个话题的子话题可能很多，所以存成列表】
+    children_topic_id_list = []
+    r_children = s.get(url = children_url, headers = Util.Default_Headers)
+    jsondata = json.loads(re.match(".*?({.*}).*", r_children.text, re.S).group(1))['data']
+    for i in jsondata:
+        children_topic_id_list.append(i['id'])
+    children_topic_id = ','.join(children_topic_id_list)
+    print('子级话题编号：', children_topic_id)
+
+    # 入库时间
+    store_time = str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+    print('入库时间：', store_time)
+
+    # 话题层级（从根话题开始为0）
+    topic_level = int(level)
+    print("话题层级：", topic_level)
+
+    topic_info = {}
+    topic_info['topic_uuid'] = topic_uuid
+    topic_info['topic_id'] = topic_id
+    topic_info['topic_name'] = topic_name
+    topic_info['topic_link'] = topic_link
+    topic_info['follow_number'] = follow_number
+    topic_info['question_number'] = question_number
+    topic_info['topic_describe'] = topic_describe
+    topic_info['parent_topic_id'] = parent_topic_id
+    topic_info['children_topic_id'] = children_topic_id
+    topic_info['store_time'] = store_time
+    topic_info['topic_level'] = topic_level
+
+    return topic_info
 
 
-# 来源于hot_url的数据
-
-r_hot = s.get(url = hot_url, headers = Util.Default_Headers)
-soup_hot = BeautifulSoup(r_hot.content, 'lxml')
-# print(soup)
-
-# uuid
-uuid = uuid.uuid1()
-print('UUID：', uuid)
-
-# 话题编号
-a = soup_hot.findAll('a', attrs={'class':'Tabs-link is-active'})
-topic_id = str(re.findall(r'.*href="/topic/(.*)/hot', str(a))[0])
-print('话题编号：', topic_id)
-# print(a)
-
-
-# 话题名称
-a = soup_hot.findAll('h1', attrs={'class':'TopicCard-titleText'})[0].contents[0]
-topic_name = str(a)
-print('话题名称：', topic_name)
-
-
-# 话题链接
-a = soup_hot.findAll('a', attrs={'class':'Tabs-link is-active'})
-topic_link = "https://www.zhihu.com" + \
-    str(re.findall(r'.*href="(.*)">', str(a))[0])
-print('话题链接：', topic_link)
-
-# 关注人数
-# print(soup_hot)
-a = soup_hot.findAll('strong', attrs={'class':'NumberBoard-itemValue'})
-follow_number = str(a[0].contents[0]).replace(',', '')
-print('关注人数：', follow_number)
-
-# 问题数
-a = soup_hot.findAll('strong', attrs={'class':'NumberBoard-itemValue'})
-question_number = str(a[1].contents[0]).replace(',', '')
-print('问题数：', question_number)
-
-
-# 来源于organize_url的数据
-# 话题别名
-# r_organize = s.get(url = organize_url, headers = Util.Default_Headers)
-# soup_organize = BeautifulSoup(r_organize.content, 'lxml')
-# print(soup_organize)
-
-# 话题描述
-a = soup_hot.findAll('span', attrs={'class':'RichText'})[0]
-topic_describe = str(re.findall(r'.*">(.*) http', str(a))[0])
-print('话题描述：', topic_describe)
-
-# 来源于parent_url的数据
-# 父级话题和子级话题不是在页面加载的，需要要请求Ajax
-# 父级话题编号【一个话题的父话题可能很多，所以存成列表】
-parent_topic_id_list = []
-r_parent = s.get(url = parent_url, headers = Util.Default_Headers)
-jsondata = json.loads(re.match(".*?({.*}).*", r_parent.text, re.S).group(1))['data']
-# print(jsondata)
-for i in jsondata:
-    parent_topic_id_list.append(i['id'])
-parent_topic_id_str = ','.join(parent_topic_id_list)
-print('父级话题编号：', parent_topic_id_str)
-
-# 来源于children_url的数据
-# 子级话题编号【一个话题的子话题可能很多，所以存成列表】
-children_topic_id_list = []
-r_children = s.get(url = children_url, headers = Util.Default_Headers)
-jsondata = json.loads(re.match(".*?({.*}).*", r_children.text, re.S).group(1))['data']
-for i in jsondata:
-    children_topic_id_list.append(i['id'])
-children_topic_id_str = ','.join(children_topic_id_list)
-print('子级话题编号：', children_topic_id_str)
-
-
-# 入库时间
-store_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-print('入库时间：', store_time)
-
-"""
-# 话题层级（从根话题开始为0）
-"""
+if __name__ == '__main__':
+    level = 0
+    number = 19704867
+    topic_info = get_topic_info(level, number)
+    print(topic_info)
---
+++ 建表语句/MySQL建表语句.sql
@@ -0,0 +1,14 @@
+﻿--话题表
+create table zhihu_topic 
+(uuid varchar(200) primary key comment '主键',
+topic_id varchar(200) comment '话题编号',
+topic_name varchar(200) comment '话题名称',
+topic_link varchar(200) comment '话题链接',
+follow_number int(10) comment '关注人数',
+question_nuber int(10) comment '问题数',
+topic_describe varchar(1000) comment '话题描述',
+parent_topic_id varchar(1000) comment '父级话题编号',
+children_topic_id varchar(1000) comment '子级话题编号',
+store_time datetime comment '入库时间',
+topic_level int(10) comment '话题层级'
+) comment = '知乎话题表';
