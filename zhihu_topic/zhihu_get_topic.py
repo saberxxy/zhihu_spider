9b59c36c460314681c11e4bc2bda14b6df2e5613
saberxxy <albert_saber@163.com>
2018-04-15 10:53:20 +0800

    按照层级抓取话题

--- zhihu_topic/zhihu_get_topic.py
+++ zhihu_topic/zhihu_get_topic.py
@@ -21,11 +21,14 @@ from bs4 import BeautifulSoup
 from zhihu_spider.zhihu_login import zhihu_login
 import zhihu_spider.zhihu_login.Util as Util
 import zhihu_spider.zhihu_login.ThreadUtil as ThreadUtil
+import zhihu_spider.common.GetMysqlConn as mysql_conn
 
+# 获取session
+s = zhihu_login.get_session()
+# 获取MySQL数据库连接
+cursor = mysql_conn.getConfig()
 
 def get_topic_info(level, number):
-    # 获取session
-    s = zhihu_login.get_session()
     # print("session：", s)
 
     hot_url = "https://www.zhihu.com/topic/" + str(number) + "/hot"
@@ -36,49 +39,49 @@ def get_topic_info(level, number):
     # 来源于hot_url的数据
     r_hot = s.get(url = hot_url, headers = Util.Default_Headers)
     soup_hot = BeautifulSoup(r_hot.content, 'lxml')
+    # print(hot_url)
+    # print(soup_hot)
 
     # uuid
     topic_uuid = str(uuid.uuid1())
-    print('UUID：', topic_uuid)
+    # print('UUID：', topic_uuid)
 
     # 话题编号
-    a_1 = soup_hot.findAll('a', attrs={'class':'Tabs-link is-active'})
-    topic_id = str(re.findall(r'.*href="/topic/(.*)/hot', str(a_1))[0])
-    print('话题编号：', topic_id)
+    topic_id = str(number)
+    # print('话题编号：', topic_id)
 
     # 话题名称
-    a_2 = soup_hot.findAll('h1', attrs={'class':'TopicCard-titleText'})[0].contents[0]
-    topic_name = str(a_2)
-    print('话题名称：', topic_name)
+    a_2 = soup_hot.findAll('title', attrs={'data-react-helmet':'true'})[0].contents[0]
+    topic_name = str(a_2).replace(' - 知乎', '').replace('话题名称： ', '')
+    # print('话题名称：', topic_name)
 
     # 话题链接
-    a_3 = soup_hot.findAll('a', attrs={'class':'Tabs-link is-active'})
-    topic_link = "https://www.zhihu.com" + \
-        str(re.findall(r'.*href="(.*)">', str(a_3))[0])
-    print('话题链接：', topic_link)
+    try:
+        # a_3 = soup_hot.findAll('a', attrs={'class':'Tabs-link is-active'})
+        topic_link = "https://www.zhihu.com/topic/" + topic_id + "/hot"
+    except Exception:
+        topic_link = 0
+    # print('话题链接：', topic_link)
 
     # 关注人数
     a_4 = soup_hot.findAll('strong', attrs={'class':'NumberBoard-itemValue'})
     follow_number = int(str(a_4[0].contents[0]).replace(',', ''))
-    print('关注人数：', follow_number)
+    # print('关注人数：', follow_number)
 
     # 问题数
     a_5 = soup_hot.findAll('strong', attrs={'class':'NumberBoard-itemValue'})
     question_number = int(str(a_5[1].contents[0]).replace(',', ''))
-    print('问题数：', question_number)
-
-    # 来源于organize_url的数据
-    # 话题别名
-    # r_organize = s.get(url = organize_url, headers = Util.Default_Headers)
-    # soup_organize = BeautifulSoup(r_organize.content, 'lxml')
-    # print(soup_organize)
+    # print('问题数：', question_number)
 
     # 话题描述
-    a_6 = soup_hot.findAll('span', attrs={'class':'RichText'})[0]
+    try:
+        a_6 = soup_hot.findAll('span', attrs={'class':'RichText'})[0]
+        topic_describe = str(a_6.contents[0])
+    except Exception:
+        topic_describe = 0
     # print(a_6)
-    # topic_describe = str(re.findall(r'.*">(.*) http', str(a_6))[0])
-    topic_describe = str(a_6.contents[0])
-    print('话题描述：', topic_describe)
+
+    # print('话题描述：', topic_describe)
 
     # 来源于parent_url的数据
     # 父级话题和子级话题不是在页面加载的，需要要请求Ajax
@@ -90,7 +93,7 @@ def get_topic_info(level, number):
     for i in jsondata:
         parent_topic_id_list.append(i['id'])
     parent_topic_id = ','.join(parent_topic_id_list)
-    print('父级话题编号：', parent_topic_id)
+    # print('父级话题编号：', parent_topic_id)
 
     # 来源于children_url的数据
     # 子级话题编号【一个话题的子话题可能很多，所以存成列表】
@@ -100,15 +103,15 @@ def get_topic_info(level, number):
     for i in jsondata:
         children_topic_id_list.append(i['id'])
     children_topic_id = ','.join(children_topic_id_list)
-    print('子级话题编号：', children_topic_id)
+    # print('子级话题编号：', children_topic_id)
 
     # 入库时间
     store_time = str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
-    print('入库时间：', store_time)
+    # print('入库时间：', store_time)
 
     # 话题层级（从根话题开始为0）
     topic_level = int(level)
-    print("话题层级：", topic_level)
+    # print("话题层级：", topic_level)
 
     topic_info = {}
     topic_info['topic_uuid'] = topic_uuid
@@ -122,12 +125,55 @@ def get_topic_info(level, number):
     topic_info['children_topic_id'] = children_topic_id
     topic_info['store_time'] = store_time
     topic_info['topic_level'] = topic_level
+    print(topic_info)
 
     return topic_info
 
 
+# 入库
+def input_mysql(topic_info):
+    # print(topic_info)
+    topic_id_exists = cursor.execute("select topic_id from zhihu_topic where topic_id=%s",
+                   (topic_info['topic_id']) )
+    # print(topic_id_exists)
+    if topic_id_exists >= 1:
+        return '话题已经存在，不再入库'
+
+    cursor.execute("insert into zhihu_topic(topic_uuid, topic_id, topic_name, topic_link,"
+            "follow_number, question_number, topic_describe, parent_topic_id,"
+            "children_topic_id, store_time, topic_level"
+            ") values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+            (topic_info['topic_uuid'], topic_info['topic_id'], topic_info['topic_name'],
+             topic_info['topic_link'], topic_info['follow_number'],
+             topic_info['question_number'], topic_info['topic_describe'],
+             topic_info['parent_topic_id'], topic_info['children_topic_id'],
+             topic_info['store_time'], topic_info['topic_level']))
+    cursor.execute("commit")
+    return '提交完毕，入库成功'
+
+
 if __name__ == '__main__':
-    level = 0
-    number = 19704867
-    topic_info = get_topic_info(level, number)
-    print(topic_info)
+    # 19555383 19776749 19618774
+    # number = 19776749
+    # 输入一个level，获取该level的下一级level的话题的子话题id
+    # level = 0
+    for level in range(3, 100):
+        print('抓取第', str(level+1), '层级的话题')
+        cursor = mysql_conn.getConfig()
+        cursor.execute("select children_topic_id, topic_level from zhihu_topic where topic_level=%s \
+                       and children_topic_id <> ''",
+                       (str(level)))
+        a = cursor.fetchall()
+
+        children_topic_id_list = []
+        children_topic_id_str = ''
+        for i in a:
+            children_topic_id_str += i['children_topic_id']+','
+        for j in eval(children_topic_id_str[:-1]):
+            topic_info = get_topic_info(level+1, j)
+            input_mysql(topic_info)
+
+
+
+
+
--- 建表语句/MySQL建表语句.sql
+++ 建表语句/MySQL建表语句.sql
@@ -1,11 +1,11 @@
 ﻿--话题表
 create table zhihu_topic 
-(uuid varchar(200) primary key comment '主键',
+(topic_uuid varchar(200) primary key comment '主键',
 topic_id varchar(200) comment '话题编号',
 topic_name varchar(200) comment '话题名称',
 topic_link varchar(200) comment '话题链接',
 follow_number int(10) comment '关注人数',
-question_nuber int(10) comment '问题数',
+question_number int(10) comment '问题数',
 topic_describe varchar(1000) comment '话题描述',
 parent_topic_id varchar(1000) comment '父级话题编号',
 children_topic_id varchar(1000) comment '子级话题编号',
